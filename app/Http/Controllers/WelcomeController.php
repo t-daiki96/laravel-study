3f8d03bae9f4d8e37d2c7fe8858da4333ad0cb6c
t-daiki96 <t.daiki96@gmail.com>
2019-02-04 13:54:33 +0900

    finish-chapter3

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,7 +10,4 @@ class WelcomeController extends Controller
     {
         return view('welcome');
     }
-    public function contact() {
-        return view('contact');
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,5 @@
 |
 */
     Route::get('/', 'WelcomeController@index');
-    Route::get('contact', 'WelcomeController@contact');
+    Route::get('contact', 'PagesController@contact');
+    Route::get('about', 'PagesController@about');
