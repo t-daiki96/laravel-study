91691c98bd3d0eb0b98784fdb07aa15a5f197658
t-daiki96 <t.daiki96@gmail.com>
2019-02-07 22:32:48 +0900

    finish-chapter14

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,5 @@
     Route::get('/', 'WelcomeController@index');
     Route::get('contact', 'PagesController@contact');
     Route::get('about', 'PagesController@about');
+    Route::get('articles', 'ArticlesController@index');
+    Route::get('articles/{id}', 'ArticlesController@show');
