de7c1bf31555457eefb850a3cf590254c6412619
vagrant <vagrant@homestead>
2019-02-08 06:59:36 +0000

    finish-chapter16

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -16,4 +16,18 @@ class ArticlesController extends Controller
         $article = Article::findOrFail($id);
         return view('articles.show', compact('article'));
     }
+
+    public function create()
+    {
+        return view('articles.create');
+    }
+    public function store() {
+        // フォームの入力値を取得
+        $inputs = \Request::all();
+        // ① マスアサインメントを使って、記事をDBに作成
+        Article::create($inputs);
+
+        // ② 記事一覧へリダイレクト
+        return redirect('articles');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@
     Route::get('contact', 'PagesController@contact');
     Route::get('about', 'PagesController@about');
     Route::get('articles', 'ArticlesController@index');
+    Route::get('articles/create', 'ArticlesController@create');
     Route::get('articles/{id}', 'ArticlesController@show');
+    Route::post('articles', 'ArticlesController@store');
